009950df97e4771680baa03b66601ce3b34aad76
Pooja <pooja@newtel.in>
2025-08-15 23:09:33 +0530

    swap number

---
+++ IdeaProjects/DSA/src/SwapNumbers.java
@@ -0,0 +1,7 @@
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+        System.out.println("swap SuccessFull");
+        System.out.println("swap was SuccessFull");
+    }
+}
